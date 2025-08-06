4c014ee6ce32ca969700740b8b112e82a6126ef6
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-08-06 11:03:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Max Value Permutation/max-value-permutation.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    int maxValue(int arr[]) {
+        int MOD = 1000000007;
+        Arrays.sort(arr);
+        long ans = 0;
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            ans = (ans + (long) arr[i] * i) % MOD;
+        }
+
+        return (int) ans;
+    }
+}
