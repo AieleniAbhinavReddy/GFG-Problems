192fd8743064429eb61dd281745dcdd34612798e
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-08-13 11:07:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Elements in the Range/elements-in-the-range.java
+++ Difficulty: Basic/Elements in the Range/elements-in-the-range.java
@@ -2,6 +2,12 @@
 
 class Solution {
     boolean check_elements(int arr[], int n, int A, int B) {
+        // int c=0;
+        // for(int i:arr){
+        //     if(i>=A && i<=B) c++;
+        // }
+        // if(c<(B-A)+1) return false;
+        // return true;
         HashSet<Integer> set=new HashSet<>();
         for(int num:arr) set.add(num);
         for(int i=A;i<=B;i++){
