18812a87d2aac63c5c91e217fd2dc3ebfef4cc64
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-04 13:47:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum Tree/sum-tree.java
@@ -0,0 +1,27 @@
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+class Solution {
+    private int checkSumTree(Node root){
+        if(root==null) return 0;
+        if(root.left==null && root.right==null) return root.data;
+        int leftSum=checkSumTree(root.left);
+        int rightSum=checkSumTree(root.right);
+        if(leftSum==-1 || rightSum==-1) return -1;
+        if(root.data != leftSum + rightSum) return -1;
+        return root.data + leftSum + rightSum;
+    }
+    boolean isSumTree(Node root) {
+        return checkSumTree(root) != -1;
+    }
+}
