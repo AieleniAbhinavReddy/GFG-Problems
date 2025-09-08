81af0ab99098c778aadb1e7ac0b5664eac9e6e28
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-08 09:48:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth largest element in BST/kth-largest-element-in-bst.java
@@ -0,0 +1,28 @@
+/*
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Solution {
+    private static List<Integer> list=new ArrayList<>();
+    public static void inorder(Node root){
+        if(root==null) return;
+        inorder(root.left);
+        list.add(root.data);
+        inorder(root.right);
+    }
+    // return the Kth largest element in the given BST rooted at 'root'
+    public int kthLargest(Node root, int k) {
+        inorder(root);
+        return list.get(list.size()-k);
+    }
+}
