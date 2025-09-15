af9ba9de5380a39e81a34347978648050dd0fba7
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-15 10:53:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Level Order Line by Line/level-order-line-by-line.java
@@ -0,0 +1,32 @@
+/*class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
+        ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+        Queue<Node> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            ArrayList<Integer> list=new ArrayList<>();
+            while(size-->0){
+                Node curr=queue.poll();
+                list.add(curr.data);
+                if(curr.left != null) queue.add(curr.left);
+                if(curr.right != null) queue.add(curr.right);
+            }
+            ans.add(list);
+        }
+        return ans;
+        
+    }
+}
