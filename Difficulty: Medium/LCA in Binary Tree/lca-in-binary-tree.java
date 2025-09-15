2e92061a9ee06572cbbe84b2d6e27307d3d2a70e
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-15 12:00:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCA in Binary Tree/lca-in-binary-tree.java
@@ -0,0 +1,23 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Solution {
+    Node lca(Node root, int n1, int n2) {
+        if(root==null) return null;
+        if(root.data==n1 || root.data==n2) return root;
+        Node leftLca=lca(root.left,n1,n2);
+        Node rightLca=lca(root.right,n1,n2);
+        if( leftLca != null &&  rightLca != null ) return root;
+        return (leftLca !=null)? leftLca : rightLca;
+    }
+}
