26409cb0574b437d00a6c203ffd5ef6338e01437
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-25 15:37:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -0,0 +1,34 @@
+class Solution {
+    private boolean hasCycle(List<List<Integer>> adj,boolean[] visited,int node,int parent){
+        visited[node]=true;
+        for(int neighbor:adj.get(node)){
+            if(!visited[neighbor]){
+                if(hasCycle(adj,visited,neighbor,node)){
+                    return true;
+                }
+            }else if(neighbor != parent){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isCycle(int V,int[][] edges){
+        //build adjacency list
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
+        for(int[] e:edges){
+            int u=e[0],v=e[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean[] visited=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(!visited[i]){
+                if(hasCycle(adj,visited,i,-1)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
