6909442a46228708ad0e0cffb067e75d1857242e
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-22 14:52:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -1,18 +1,19 @@
 // User function Template for Java
 
 class Solution {
-    static int checkOnes(int[] arr){
-        int count=0;
-        for(int n:arr) if(n==1) count++;
-        return count;
-    }
     public int rowWithMax1s(int arr[][]) {
-        ArrayList<Integer> count=new ArrayList<>();
-        for(int[] suba:arr){
-            count.add(checkOnes(suba));
-        }
         int max=0;
-        for(int i=0;i<count.size();i++) if(count.get(i)>count.get(max)) max=i;
-        return max;
+        for(int i=0;i<arr.length;i++){
+            int c=0;
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==1) c++;
+            }
+            max=Math.max(max,c);
+            arr[i][0]=c;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i][0]==max) return i; 
+        }
+        return -1;
     }
 }
