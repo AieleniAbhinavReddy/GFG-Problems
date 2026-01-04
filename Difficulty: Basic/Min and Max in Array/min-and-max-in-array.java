340cb224470313ea93c1e537822c15ee085a1bac
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-04 12:48:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -1,37 +1,43 @@
-// User function Template for Java
-// User function Template for Java
-
-/*
-class Pair<K, V> {
-    private final K key;
-    private final V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-}
-
-Java users need to return result in Pair class
-For Example -> return new Pair(minimum,maximum)
-*/
-
 class Solution {
-    public Pair<Integer, Integer> getMinMax(int[] arr) {
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        ArrayList<Integer> res=new ArrayList<>();
         int min=arr[0],max=arr[0];
         for(int n:arr){
-            if(n>max) max=n;
-            if(n<min) min=n;
+            if(min>n){
+                min=n;
+            }
+            if(max<n){
+                max=n;
+            }
         }
-        Pair<Integer,Integer> res=new Pair<>(min,max);
+        res.add(min);
+        res.add(max);
         return res;
     }
 }
+
+/*
+Approach :
+
+use two variables min=1st value and max= 1st value
+a normal for loop
+using two conditions 
+one condition is min>curr -> min=curr
+another condition is max<curr -> max=curr
+
+arr = [12, 3, 15, 7, 9]
+
+min=12 , max=12
+
+12 -> min>12 -> nothing -> max<12 -> nothing
+3 -> min>3 -> min=3 -> max<3 -> nothing
+15 -> min>15 -> min=3 -> max<15 -> max=15
+7 -> min>7 -> min=3 -> max<7 -> max=15
+9 -> min>9 -> min=3 -> max<9 -> max=15
+
+min =3 , max=15
+res = {min,max}
+
+output res
+
+*/
