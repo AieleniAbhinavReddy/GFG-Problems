e034d6742aaedc1d1c0b855b5fb0657af230eda5
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-05 21:52:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -7,9 +7,18 @@ class Solution {
         if(n==1) return res;
         res[0]=0;
         res[1]=1;
-        for(int i=2;i<res.length;i++){
+        for(int i=2;i<n;i++){
             res[i]=res[i-1]+res[i-2];
         }
         return res;
     }
 }
+/*
+Fibonacci numbers : they are a series of numbers which are generated using a logic
+fibonacci number sequence starts from a=0 , b=1
+formula for generating fibonacci numbers : c=a+b
+
+n=5
+0 1 1 2 3
+
+*/
