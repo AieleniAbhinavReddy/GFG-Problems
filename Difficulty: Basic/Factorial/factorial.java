78ab8416312e32b50b575a6f7d819f30ac2ef87a
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-05 22:10:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -2,7 +2,15 @@ class Solution {
     // Function to calculate factorial of a number.
     int factorial(int n) {
         int res=1;
-        for(int i=2;i<=n;i++) res*=i;
+        for(int i=2;i<=n;i++){
+            res=res*i;
+        }
         return res;
     }
 }
+/*
+Factorial : it is product of all the numbers from 1 to n for a integer n
+
+n=5
+factorial of 5 = 1 X 2 X 3 X 4 X 5 = 120
+*/
