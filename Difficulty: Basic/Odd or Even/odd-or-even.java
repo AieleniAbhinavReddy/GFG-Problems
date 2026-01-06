2044077af06861881c065fd07ce916f6c07d5e46
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-06 19:50:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Odd or Even/odd-or-even.java
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -1,11 +1,18 @@
 class Solution {
     static boolean isEven(int n) {
-        // code here
-        if(n%2==0){
+        if(n%2 == 0){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
 }
+
+/*
+Approach : reminder = 0 -> even
+% -> it gives us the reminder of two numbers division
+a/b -> c
+c=a&b;
+
+n%2 = reminder -> 0 -> even
+n%2 = reminder -> 1 -> odd
+*/
