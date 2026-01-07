d85eb4482e2173172636be7ae308567b2e7a4402
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-07 21:58:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.java
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.java
@@ -13,8 +13,22 @@ class Solution {
     public Node insertAtEnd(Node head, int x) {
         if(head==null) return new Node(x);
         Node curr=head;
-        while(curr.next != null) curr=curr.next;
-        curr.next=new Node(x);
+        while(curr.next!=null){
+            curr=curr.next;
+        }
+        Node newNode=new Node(x);
+        curr.next=newNode;
         return head;
     }
 }
+/*
+Approach:
+variable
+loop
+
+variable = curr
+loop = to iterate through the list until we reach the end
+and at last point the curr to new node
+
+head which they gave us
+*/
