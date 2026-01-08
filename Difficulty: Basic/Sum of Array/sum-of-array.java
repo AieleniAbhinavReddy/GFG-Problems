5c128378395279b8de927187c2c13d8fb6da7d40
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-08 21:53:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    int arraySum(int arr[]) {
+        int sum=0;
+        for(int n:arr){
+            sum+=n;//sum=sum+n;
+        }
+        return sum;
+    }
+}
+/*
+Approach
+variable
+loop
+
+loop -> we will iterate numbers from array one by one
+variable -> sum in which we will add each and every value from array
+*/
