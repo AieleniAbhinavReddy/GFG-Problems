593eccbb93b04145e11b01d1986728fc82418d02
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-08 21:49:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Squares/count-squares.java
+++ Difficulty: Basic/Count Squares/count-squares.java
@@ -2,6 +2,12 @@
 
 class Solution {
     static int countSquares(int n) {
-        return (int)Math.sqrt(n - 1);
+        int res=(int)Math.sqrt(n-1);
+        return res;
     }
 }
+/*
+Approach
+
+formula = no.of perfect squares less than n = sqrt(n-1);
+*/
