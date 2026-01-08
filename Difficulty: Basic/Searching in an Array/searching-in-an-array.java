f271ffc3d642a99d4bd2b50073e4f8163c65a964
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-08 21:39:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Searching in an Array/searching-in-an-array.java
+++ Difficulty: Basic/Searching in an Array/searching-in-an-array.java
@@ -2,8 +2,20 @@
 class Solution {
     public int search(int k, ArrayList<Integer> arr) {
         for(int i=0;i<arr.size();i++){
-            if(arr.get(i)==k) return i+1;
+            if(arr.get(i)==k){
+                return i+1;
+            }
         }
         return -1;
     }
 }
+/*
+Approach
+loop 
+condition
+
+loop -> iterate the given list sequentially
+condition -> to check if current value is equal to k
+if equal -> output is the index
+k is not found anywhere in arr -> -1
+*/
