6eb6206c071b3cbed7a6b459081e81458d32a9aa
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-08 14:50:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Insert in Middle of Linked List/insert-in-middle-of-linked-list.java
@@ -0,0 +1,44 @@
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node insertInMiddle(Node head, int x) {
+        if(head==null) return new Node(x);
+        int len=0;
+        Node curr=head;
+        while(curr!=null) {
+            len++;
+            curr=curr.next;
+        }
+        Node slow=head;
+        Node fast=head;
+        Node prev=null;
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        if(len%2==0){
+            Node temp=prev.next;
+            Node newn=new Node(x);
+            prev.next=newn;
+            newn.next=temp;
+        }else{
+            Node temp=slow.next;
+            Node newn=new Node(x);
+            slow.next=newn;
+            newn.next=temp;
+        }
+        return head;
+    }
+}
