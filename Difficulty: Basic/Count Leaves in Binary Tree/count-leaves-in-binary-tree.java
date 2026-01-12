1ecd84d2694f3ebe9d52909f1dd320d4f72d797b
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-01-12 16:27:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.java
+++ Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.java
@@ -6,16 +6,32 @@ class Node
 }*/
 
 class Solution {
-    private static int c;
-    private static void helper(Node root){
+    static int count=0;
+    void preorder(Node root){
         if(root==null) return;
-        if(root.left==null&&root.right==null) c++;
-        helper(root.left);
-        helper(root.right);
+        if(root.left==null && root.right==null) count++;
+        preorder(root.left);
+        preorder(root.right);
     }
     int countLeaves(Node node) {
-        c=0;
-        helper(node);
-        return c;
+        count=0;
+        preorder(node);
+        return count;
     }
 }
+/*
+Approach
+preorder algorithm to traverse the entire tree
+leaf node = the left child and right child of the node are null
+count = to cound the leaf nodes
+
+preorder :
+1 -> not leaf
+10 -> not leaf
+5 -> leaf node -> c=1
+39 -> leaf node -> c=2
+
+output is 2
+
+
+*/
