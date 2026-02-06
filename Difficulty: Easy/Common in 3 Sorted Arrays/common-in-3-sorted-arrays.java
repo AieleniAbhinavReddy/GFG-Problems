0b79c2a66a413812dc4709ae5fe84779bb729f26
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-02-06 19:40:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -1,23 +1,18 @@
-// User function Template for Java
-
 class Solution {
-    static boolean find(List<Integer> arr,int t){
-        int l=0,r=arr.size()-1;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            int c=arr.get(m);
-            if(c==t) return true;
-            else if(c>t) r=m-1;
-            else l=m+1;
-        }
-        return false;
-    }
     public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2,List<Integer> arr3) {
         List<Integer> res=new ArrayList<>();
-        for(int n:arr1){
-            if(find(arr2,n) && find(arr3,n)){
-                if(!res.contains(n)) res.add(n);
-            }
+        int i=0,j=0,k=0,n1=arr1.size(),n2=arr2.size(),n3=arr3.size();
+        while(i<n1 && j<n2 && k<n3){
+            int a=arr1.get(i),b=arr2.get(j),c=arr3.get(k);
+            if(a==b && b==c){
+                if(!res.contains(a)) res.add(a);
+                i+=1;
+                j+=1;
+                k+=1;
+            } 
+            else if(a<=b && a<=c) i+=1;
+            else if(b<=a && b<=c) j+=1;
+            else k+=1;
         }
         return res;
     }
