7beb974a652094f30141b7e54c68ab99aad8309f
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2026-02-06 20:26:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.java
@@ -1,19 +1,37 @@
 class Solution {
     public List<Integer> commonElements(List<Integer> arr1, List<Integer> arr2,List<Integer> arr3) {
         List<Integer> res=new ArrayList<>();
-        int i=0,j=0,k=0,n1=arr1.size(),n2=arr2.size(),n3=arr3.size();
-        while(i<n1 && j<n2 && k<n3){
+        int i=0,j=0,k=0,l1=arr1.size(),l2=arr2.size(),l3=arr3.size();
+        while(i<l1 && j<l2 && k<l3){
             int a=arr1.get(i),b=arr2.get(j),c=arr3.get(k);
             if(a==b && b==c){
                 if(!res.contains(a)) res.add(a);
-                i+=1;
-                j+=1;
-                k+=1;
-            } 
-            else if(a<=b && a<=c) i+=1;
-            else if(b<=a && b<=c) j+=1;
-            else k+=1;
+                i++;
+                j++;
+                k++;
+            }else if(a<=b && a<=c) i++;
+            else if(b<=a && b<=c) j++;
+            else k++;
         }
         return res;
     }
 }
+/*
+Approach:
+things used:
+loops
+conditions
+array list
+variables
+
+steps:
+1)declare the variables to track current index for all arrays
+2)while loop to iterate all the 3 arrays at a time
+3)check if the 3 current elements are equal
+4)then add one element to result and increment all 3 index variables
+5)if arr 1 element is smallest then increment it by one
+6)if arr 2 element is smallest then increment it by one
+7)so increment arr 3 index by one
+8)we got out output in the res array to return it as output
+
+*/
