c1eaea9c988fe386ed744bac33986d862734784f
AieleniAbhinavReddy <aeileniabhinavreddy@gmail.com>
2026-03-10 21:25:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverse(String S) {
+        // code here
+        Stack<Character> st=new Stack<>();
+        for(char c:S.toCharArray()){
+            st.push(c);
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+}
